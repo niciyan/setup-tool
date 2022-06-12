0d6a484519e5473dd6c35c238b2a21edeb4377c7
niciyan <nishiyamasu@gmail.com>
2022-06-12 14:31:24 +0900

    added echo

--- ubuntu-gcp.sh
+++ ubuntu-gcp.sh
@@ -5,3 +5,8 @@ sudo apt install language-pack-ja
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 sudo -s source /etc/default/locale
 sudo timedatectl set-timezone Asia/Tokyo
+cat << EOF
+You may want to run commands below to refresh locale:
+sudo -s
+source /etc/default/locale
+EOF
