526fd6f5ebc51e6c50532cca38789ec82d12bfaf
niciyan <nishiyamasu@gmail.com>
2022-06-12 14:19:53 +0900

    added timezone changing

--- ubuntu-gcp.sh
+++ ubuntu-gcp.sh
@@ -4,3 +4,4 @@ sudo apt update
 sudo apt install language-pack-ja
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 sudo -s source /etc/default/locale
+sudo timedatectl set-timezone Asia/Tokyo
