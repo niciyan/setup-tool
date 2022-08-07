fcc36add55472c22d808ad1fb1f35a23354df8dc
niciyan <nishiyamasu@gmail.com>
2022-08-07 23:42:01 +0900

    Modified install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -ex
 
+# :WARNING:
+# you may need to delete previous golang files
+# rm -rf /usr/local/go
 
+GO_DIR=/usr/local/go
 # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
 sudo  tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-ls /usr/local/go/
+echo listing $GO_DIR
+ls -l $GODIR
 export PATH=$PATH:/usr/local/go/bin
 
