f64cd88e9eb8ce03cfd6dee9aff0b5141fba256d
niciyan <nishiyamasu@gmail.com>
2022-08-17 10:20:24 +0900

    Modified install-go script

--- install-go.sh
+++ install-go.sh
@@ -9,7 +9,7 @@ GO_DIR=/usr/local/go
 # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
 sudo  tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-echo listing $GO_DIR
-ls -l $GODIR
 export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+go version
 
