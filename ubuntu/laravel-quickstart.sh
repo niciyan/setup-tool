3e959eb271dd08a757c50b5d1077197fd58ff2f8
niciyan <nishiyamasu@gmail.com>
2023-06-12 00:12:12 +0900

    Add: added script for laravel quickstart

---
+++ ubuntu/laravel-quickstart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install -y php php-xml
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+composer --version
+
+composer create-project laravel/laravel:^8.0 example-app
+cd example-app/
+php artisan serve
+
